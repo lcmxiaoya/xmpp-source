4bbfcdf7bf3a889a28019668258720b5ecb8ece1
lcmxiaoya <lcmxiaoya@163.com>
2018-09-18 17:17:27 +0800

    修改自发自收不成功时，无法重连的问题。

--- jsxmpp/CommunicationManager.cs
+++ jsxmpp/CommunicationManager.cs
@@ -125,7 +125,7 @@
                 {
                     if (!this.IsXmppOK)
                     {
-                        CommonConfig.Logger.WriteInfo(string.Format("连接状态异常，关闭连接：IsHasRosterOnline:{0},Connected:{1},m_sendErrorTimes:{2}", this.m_xmppClient.IsHasRosterOnline, this.m_xmppClient.Connected, this.m_sendErrorTimes));
+                        CommonConfig.Logger.WriteInfo(string.Format("连接状态异常，关闭连接：IsHasRosterOnline:{0},Connected:{1},m_sendErrorTimes:{2}", this.m_xmppClient != null ? this.m_xmppClient.IsHasRosterOnline : false, this.m_xmppClient != null ? this.m_xmppClient.Connected : false, this.m_sendErrorTimes));
                         if (this.ReconnectTimes > 2)
                         {
                             CommonConfig.Logger.WriteInfo("连续重连两次不成功，暴力退出！");
@@ -438,7 +438,7 @@
                         ServiceResponseData responseData = new ServiceResponseData
                         {
                             seqId = requestParam.seqId,
-                            message = message,
+                            message = "",
                             resultCode = 0,
                             serviceId = m_serviceIdForSelf 
                         };
